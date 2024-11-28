be8666631277181b9c596a397f598d053d820beb
vicky <vickykumarsunilja@gmail.com>
2024-11-28 13:39:22 +0530

    Updated the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
 import express, {Request, Response} from "express";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(process.cwd(), "../frontend/dist")));
+
 app.get("/api", (req: Request, res: Response) => {
   res.json({message: "Hello from the Backend!"});
+});
+
+//This should be after all api routes
+//This will solve manual refresh issue
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {
