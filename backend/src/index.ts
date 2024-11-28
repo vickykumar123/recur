ed4d3f85476aa6c5dde138fd34ae15194bc9cbed
vicky <vickykumarsunilja@gmail.com>
2024-11-28 15:42:53 +0530

    Updated the package.json file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(process.cwd(), "../frontend/.next")));
+app.use(express.static(path.join(__dirname, "../../frontend/out")));
 
 app.get("/api", (req: Request, res: Response) => {
   res.json({message: "Hello from the Backend!"});
@@ -13,7 +13,7 @@ app.get("/api", (req: Request, res: Response) => {
 //This should be after all api routes
 //This will solve manual refresh issue
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../frontend", ".next"));
+  res.sendFile(path.join(__dirname, "../../frontend/out", "index.html"));
 });
 
 app.listen(PORT, () => {
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  distDir: "out", // Default, but ensure this matches your Express configuration
 };
 
 export default nextConfig;
